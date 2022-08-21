215f6c1314b653ff8eb8e458baff8b8d4575e6b9
UP957417 <91566990+UP957417@users.noreply.github.com>
2022-08-21 15:30:10 +0100

    added corner=True option

--- Implementations/pairplot/pairplot_implement.py
+++ Implementations/pairplot/pairplot_implement.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 class Pairplot:
-    def __init__(self, df, vars=None, corner=False, hue=None):
+    def __init__(self, df, vars=None, corner=True, hue=None):
+        self.flag = corner
         self.data = df
         self.vars = 0
         if vars is None:
@@ -11,8 +12,28 @@ class Pairplot:
             self.vars = vars
         self.x = len(self.vars)
         self.fig, self.axes = plt.subplots(nrows=self.x, ncols=self.x)
-    
+
     def draw(self):
+        if self.flag:
+            self.draw_corner()
+        else:
+            self.draw_no_corner()
+
+    def draw_no_corner(self):
+        axes = self.axes
+        x = self.vars
+        y = self.vars
+        for i in range(len(x)):
+            for j in range(len(y)):
+                if x[i] == y[j]:
+                    axes[i][j].hist(self.data[x[i]], bins=15)
+                elif j > i:
+                    axes[i][j].axis("off")
+                else:
+                    axes[i][j].scatter(self.data[x[i]], self.data[y[j]])
+        plt.show()
+    
+    def draw_corner(self):
         axes = self.axes
         x = self.vars
         y = self.vars
