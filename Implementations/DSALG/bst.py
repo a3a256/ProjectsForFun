91f254a3554c94dde48387ab8282b19fc75c43ac
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-08-23 23:21:25 +0100

    BinaryTree

---
+++ Implementations/DSALG/bst.py
@@ -0,0 +1,62 @@
+class Tree:
+    def __init__(self, root):
+        self.root = root
+        self.left = None
+        self.right = None
+        self.arr = []
+
+    def add(self, value):
+        if self.root:
+            if self.root == value:
+                return
+            else:
+                if value < self.root:
+                    if self.left:
+                        self.left.add(value)
+                    else:
+                        self.left = Tree(value)
+                else:
+                    if self.right:
+                        self.right.add(value)
+                    else:
+                        self.right = Tree(value)
+        else:
+            self.root = Tree(value)
+        
+    def iot(self, root):
+        if root:
+            self.iot(root.left)
+            self.arr.append(root.root)
+            self.iot(root.right)
+        
+    def pot(self, root):
+        if root:
+            self.arr.append(root.root)
+            self.pot(root.right)
+            self.pot(root.left)
+
+    def pot_1(self, root):
+        if root:
+            self.pot_1(root.left)
+            self.pot_1(root.right)
+            self.arr.append(root.root)
+        
+    def show(self):
+        arr = self.arr
+        self.arr = []
+        return arr
+
+
+if __name__ == '__main__':
+    t = Tree(5)
+    t.add(2)
+    t.add(1)
+    t.add(7)
+    t.add(6)
+    t.add(8)
+    t.iot(t)
+    print(t.show())
+    t.pot(t)
+    print(t.show())
+    t.pot_1(t)
+    print(t.show())
