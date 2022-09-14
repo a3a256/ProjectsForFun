6c38e86b354a93a45d2dde37441b20019f58978c
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-09-14 14:15:41 +0100

    added binary search tree to the 'app'

---
+++ algorithms_app/bst_fun.py
@@ -0,0 +1,67 @@
+class Binary:
+    def __init__(self, root, ui, x, y, height=16):
+        self.root = root
+        self.left = None
+        self.right = None
+        self.ui = ui
+        self.ui.create_text(x, y, text=str(self.root),font=('Helvetica', str(height), 'bold'))
+        self.ui.pack()
+        self.vals = []
+        
+
+    def add(self, value, text_x, text_y, line_x, line_y, coef=200):
+        if self.root is None:
+            self.root = Binary(value, None, None)
+            return
+        else:
+            if value >= self.root:
+                if self.right:
+                    self.right.add(value, text_x+coef, text_y+70, line_x+coef, line_y+70, coef=coef-(coef//2))
+                else:
+                    self.ui.create_line((line_x), line_y+5, (line_x)+coef, (line_y+50), width=1.5)
+                    self.right = Binary(value, self.ui, text_x+coef, text_y+80)
+            else:
+                if self.left:
+                    self.left.add(value, text_x-coef, text_y+70, line_x-coef, line_y+70, coef=coef-(coef//2))
+                else:
+                    self.ui.create_line(line_x, line_y+5, line_x-coef, line_y+50, width=1.5)
+                    self.left = Binary(value, self.ui, text_x-coef, text_y+80)
+
+    def pot(self, tree):
+        if tree:
+            self.vals.append(tree.root)
+            self.pot(tree.left)
+            self.pot(tree.right)
+
+    def iot(self, tree):
+        if tree:
+            self.iot(tree.left)
+            self.vals.append(tree.root)
+            self.iot(tree.right)
+
+    def post(self, tree):
+        if tree:
+            self.post(tree.left)
+            self.post(tree.right)
+            self.vals.append(tree.root)
+
+    def show(self):
+        l = ''
+        for i in self.vals:
+            l += str(i) + ', '
+        self.vals = []
+        return l[:-2]
+
+if __name__ == '__main__':
+    bn = Binary(5)
+    bn.add(3)
+    bn.add(4)
+    bn.add(2)
+    bn.add(7)
+    bn.add(6)
+    bn.add(8)
+    bn.pot(bn)
+    print('-'*10)
+    bn.iot(bn)
+    print('-'*10)
+    bn.post(bn)
---
+++ algorithms_app/to_portray_bst.py
@@ -0,0 +1,64 @@
+from tkinter import *
+import bst_fun
+
+class Activated:
+    def __init__(self, ui, first):
+        self.gui = ui
+        self.gui.geometry('1000x1000')
+        self.cv = Canvas(width=1000, height=1000)
+        self.cv.pack(fill='both', expand=True)
+        self.tree = bst_fun.Binary(first, self.cv, 400, 25)
+        self.intake = StringVar()
+        self.intake.set('')
+        self.traverse = StringVar()
+
+
+    def adding(self, num):
+        n = int(num)
+        self.tree.add(n, 400, 25, 400, 40)
+        self.intake.set('')
+
+
+    def in_order(self):
+        self.tree.iot(self.tree)
+        outage = self.tree.show()
+        label = Label(master=self.gui, text=f'InOrderTraversal: {outage}', font=('Helvetica', 16))
+        self.cv.create_window(200, 600, window=label)
+
+
+    def pre_order(self):
+        self.tree.iot(self.tree)
+        outage = self.tree.show()
+        label = Label(master=self.gui, text=f'PreOrderTraversal: {outage}', font=('Helvetica', 16))
+        self.cv.create_window(205, 630, window=label)
+
+
+    def post_order(self):
+        self.tree.iot(self.tree)
+        outage = self.tree.show()
+        label = Label(master=self.gui, text=f'PostOrderTraversal: {outage}', font=('Helvetica', 16))
+        self.cv.create_window(210, 660, window=label)
+        
+
+
+    def show(self):
+        ent = Entry(master=self.gui, text=self.intake)
+        ent_window = self.cv.create_window(900,50, window=ent)
+        btn = Button(master=self.gui, text="Add number", command=lambda: self.adding(ent.get()))
+        btn_window = self.cv.create_window(900,80, window=btn)
+        tr_lb = Label(master=self.gui, text="Traverse!")
+        tr_lb_window = self.cv.create_window(900, 150, window=tr_lb)
+        iot_btn = Button(master=self.gui, text="InOrderTraversal", command=lambda: self.in_order())
+        iot_btn_window = self.cv.create_window(900, 180, window=iot_btn)
+        pre_btn = Button(master=self.gui, text="PreOrderTraversal", command=lambda: self.pre_order())
+        pre_btn_window = self.cv.create_window(900, 210, window=pre_btn)
+        post_btn = Button(master=self.gui, text="PostOrderTraversal", command=lambda: self.post_order())
+        post_btn_window = self.cv.create_window(900, 240, window=post_btn)
+        self.gui.mainloop()
+
+
+
+if __name__ == '__main__':
+    u = Activated(Tk(), 10)
+    u.show()
+        
