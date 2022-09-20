482c2f8850e3969253460976955be7fe77e89847
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-09-20 22:25:59 +0100

    updated tkinter askopenfile

--- ml_app/get_path.py
+++ ml_app/get_path.py
@@ -1,9 +1,34 @@
 from tkinter import *
-
+from tkinter import ttk, filedialog
+from tkinter.filedialog import askopenfile
+import os
+import tables
 
 class BrowseFile:
     def __init__(self, ui):
         self.ui = ui
+        self.ui.geometry('800x600')
+        self.cv = Canvas(width=800, height=600)
+        self.cv.pack(fill='both', expand=True)
 
     def file_path(self):
         _filename = filedialog.askopenfilename()
+        _filename = r'{}'.format(_filename)
+        root = Tk()
+        tables.Example(root, _filename).pack(fill='both', expand=True)
+        root.mainloop()
+
+
+    def port(self):
+        click = Button(master=self.ui, text='Open File', command=lambda: self.file_path())
+        self.cv.create_window(40, 10, window=click)
+        self.ui.mainloop()
+
+
+def main():
+    f = BrowseFile(Tk())
+    f.port()
+
+
+if __name__ == '__main__':
+    main()
