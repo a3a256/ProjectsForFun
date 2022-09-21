94ee9c4bb205409a494626e046280245d0e20bf0
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-09-21 10:45:46 +0100

    major amendments for dataset visualisation

--- ml_app/tables.py
+++ ml_app/tables.py
@@ -3,42 +3,24 @@ from tkinter import ttk, filedialog
 import pandas as pd
 
 class DataVis(Frame):
-    def __init__(self, path, ui):
-        Frame.__init__(self, ui)
-        self.text = Text(self, wrap='none')
-        self.h = Scrollbar(orient='horizontal', command=self.text.xview)
-        self.text.configure(xscrollcommand=self.h.set)
-        self.h.pack(side=BOTTOM, fill='x')
-        self.text.pack(fill='both', expand=True)
-        self.df = pd.read_csv(path)
-        self.cols = dict()
-
-    def listen(self, val):
-        print(val)
-        
-    def draw(self):
-        cols = self.df.columns
-        for col in cols:
-            self.cols[col] = Button(self, text=col, command=self.listen(col))
-            self.text.window_create(self.cols[col])
-            self.text.insert('end', '')
-        self.text.configure(state='disabled')
-        self.mainloop()
-
-class Example(Frame):
     def __init__(self, parent, path):
         Frame.__init__(self, parent)
         text = Text(self, wrap="none")
         vsb = Scrollbar(self, orient="horizontal", command=text.xview)
+        vsb1 = Scrollbar(self, orient='vertical', command=text.yview)
         text.configure(xscrollcommand=vsb.set)
+        text.configure(yscrollcommand=vsb1.set)
         vsb.pack(side=BOTTOM, fill="x")
+        vsb1.pack(side='right', fill="y")
         text.pack(fill="both", expand=True)
         df = pd.read_csv(path)
         cols = df.columns
         col_lengths = []
+        cls = []
         for i in cols:
             ls = [len(str(x)) for x in df.loc[:, i]]
             col_lengths.append(max(ls))
+            cls.append(len(str(i)))
         j = 0
         for col in cols:
             b = Button(self, text=col)
@@ -50,12 +32,15 @@ class Example(Frame):
         text.insert("end", "\n")
 
         for i in range(20):
-            v = 0
+            v = 1
             for j in range(len(cols)):
                 lb = Label(self, text=df.iloc[i, j])
                 text.window_create("end", window=lb)
-                text.insert("end", " "*col_lengths[v])
-                v += 0
+                if v < len(cls)-1:
+                    text.insert("end", " "*(cls[v]))
+                else:
+                    text.insert("end", "")
+                v += 1
             text.insert("end", "\n")
 
         text.configure(state="disabled")
@@ -65,5 +50,5 @@ class Example(Frame):
 
 if __name__ == "__main__":
     root = Tk()
-    t = Example(root, r'C:\Users\Azamat.Ilyasov\Downloads\breast-cancer.csv').pack(fill='both', expand=True)
+    t = DataVis(root, r'C:\Users\Azamat.Ilyasov\Downloads\breast-cancer.csv').pack(fill='both', expand=True)
     root.mainloop()
