93b744a3c210542d2c94e064652536439cf4bf0c
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-09-22 14:59:24 +0100

    added some VERY simple derivative calculator

---
+++ derivative calculator/backend.py
@@ -0,0 +1,86 @@
+from tkinter import E
+
+
+class Distribute:
+    def __init__(self, equation, target):
+        self.target = target
+        self.equation = equation[5:]
+        self.rule = ["+", "-", "*", "/"]
+        self.operands = []
+
+    def process(self):
+        l = list(self.equation)
+        for i in l:
+            if i == ' ':
+                l.remove(i)
+        l = "".join(l)
+        self.equation = l
+
+    def extract_nums(self):
+        vals = []
+        nums = ""
+        self.process()
+        for i in self.equation:
+            if i in self.rule:
+                self.operands.append(i)
+                vals.append(nums)
+                nums = ""
+            else:
+                nums += i
+        vals.append(nums)
+        return vals
+
+    def clear(self, val):
+        while "_" in val:
+            val.remove("-")
+        return val
+
+    def get_num(self, val):
+        weight = []
+        i = 0
+        while val[i] != "x":
+            weight.append(val[i])
+            i += 1
+        if not weight:
+            return "1"
+        return "".join(weight)
+
+    def extract_exponent(self, val):
+        length = len(val)
+        end = length-2
+        e = ""
+        while val[end] != "(":
+            e += val[end]
+            end -= 1
+        return e
+
+    def is_exponent(self, val):
+        if "^" in val:
+            return True
+        return False
+
+    def derive_one_num(self, val):
+        if val == self.target:
+            return "1"
+        if self.target not in val:
+            return "0"
+        weight = int(self.get_num(val))
+        if not self.is_exponent(val):
+            return str(weight)
+        powers = int(self.extract_exponent(val))
+        new_weight = str(weight*powers)
+        new_powers = str(powers-1)
+        return str(str(new_weight)+str(self.target)+"^("+str(new_powers)+")")
+
+    def derive_full(self):
+        vals = self.extract_nums()
+        new_vals = []
+        for i in vals:
+            new_vals.append(self.derive_one_num(i))
+        print(new_vals)
+        new_expression = new_vals[0]
+        if len(new_vals) > 1:
+            for i in range(1, len(new_vals)):
+                new_expression += self.operands[i-1] + new_vals[i]
+        print(new_expression)
+        return new_expression
---
+++ derivative calculator/calc.py
@@ -0,0 +1,93 @@
+from tkinter import *
+import backend
+import time
+
+
+class Calculations:
+    def __init__(self, ui):
+        self.ui = ui
+        self.intake = "f()="
+        self.equation = StringVar()
+        self.equation.set(self.intake)
+        self.arg = ""
+
+    def num_press(self, val):
+        self.intake = self.equation.get()
+        if val == "x":
+            if self.intake[2] == ")":
+                win = Tk()
+                lb = Label(master=win, text="Error! Enter argument into f() function, inout is cleared!")
+                lb.pack()
+                self.intake = "f()="
+                self.equation.set(self.intake)
+                return
+            else:
+                if self.intake[2] != "e":
+                    self.arg = self.intake[2]
+                else:
+                    win = Tk()
+                    lb = Label(master=win, text="Unfortunately this character is an exponent, enter another character as an argument")
+                    lb.pack()
+                    self.intake = "f()="
+                    self.equation.set(self.intake)
+        self.intake += val
+        self.equation.set(self.intake)
+
+    def op_press(self, val):
+        self.intake = self.equation.get()
+        self.intake += val
+        self.equation.set(self.intake)
+
+    def equal(self):
+        bb = backend.Distribute(self.equation.get(), self.arg)
+        bb.derive_full()
+
+    def exp(self):
+        self.intake += "^()"
+        self.equation.set(self.intake)
+
+    def activate(self):
+        en1 = Entry(master=self.ui, text=self.equation)
+        en1.grid(row=0, columnspan=4)
+        btn1 = Button(master=self.ui, text="1", command=lambda: self.num_press("1"))
+        btn1.grid(row=1, column=0)
+        btn2 = Button(master=self.ui, text="2", command=lambda: self.num_press("2"))
+        btn2.grid(row=1, column=1)
+        btn3 = Button(master=self.ui, text="3", command=lambda: self.num_press("3"))
+        btn3.grid(row=1, column=2)
+        btn4 = Button(master=self.ui, text="4", command=lambda: self.num_press("4"))
+        btn4.grid(row=2, column=0)
+        btn5 = Button(master=self.ui, text="5", command=lambda: self.num_press("5"))
+        btn5.grid(row=2, column=1)
+        btn6 = Button(master=self.ui, text="6", command=lambda: self.num_press("6"))
+        btn6.grid(row=2, column=2)
+        btn7 = Button(master=self.ui, text="7", command=lambda: self.num_press("7"))
+        btn7.grid(row=3, column=0)
+        btn8 = Button(master=self.ui, text="8", command=lambda: self.num_press("8"))
+        btn8.grid(row=3, column=1)
+        btn9 = Button(master=self.ui, text="9", command=lambda: self.num_press("9"))
+        btn9.grid(row=3, column=2)
+        btn_plus = Button(master=self.ui, text="+", command=lambda: self.op_press("+"))
+        btn_plus.grid(row=1, column=3)
+        btn_plus = Button(master=self.ui, text="-", command=lambda: self.op_press("-"))
+        btn_plus.grid(row=2, column=3)
+        btn_mul = Button(master=self.ui, text="*", command=lambda: self.op_press("*"))
+        btn_mul.grid(row=3, column=3)
+        btn_dot = Button(master=self.ui, text='x', command=lambda: self.num_press("x"))
+        btn_dot.grid(row=4, column=1)
+        btn0 = Button(master=self.ui, text="0", command=lambda: self.num_press("0"))
+        btn0.grid(row=4, column=0)
+        btn_equal = Button(master=self.ui, text="=", command=lambda: self.equal())
+        btn_equal.grid(row=4, column=2)
+        btn_pow = Button(master=self.ui, text="^", command=lambda: self.exp())
+        btn_pow.grid(row=4, column=3)
+        self.ui.mainloop()
+
+
+def main():
+    c = Calculations(Tk())
+    c.activate()
+
+
+if __name__ == '__main__':
+    main()
