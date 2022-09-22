7aef3f3507887f8fe2887b67d536045476158086
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-09-22 17:18:51 +0100

    added beta version of multiplication in derivs

--- derivative calculator/backend.py
+++ derivative calculator/backend.py
@@ -38,9 +38,12 @@ class Distribute:
     def get_num(self, val):
         weight = []
         i = 0
-        while val[i] != "x":
-            weight.append(val[i])
-            i += 1
+        if self.target in val:
+            while val[i] != self.target:
+                weight.append(val[i])
+                i += 1
+        else:
+            return val
         if not weight:
             return "1"
         return "".join(weight)
@@ -74,13 +77,41 @@ class Distribute:
             return str(str(new_weight) + str(self.target))
         return str(str(new_weight)+str(self.target)+"^("+str(new_powers)+")")
 
+    def derivative_mul(self, one, two):
+        one_weight = str(self.get_num(one))
+        two_weight = str(self.get_num(two))
+        one_power = 0
+        two_power = 0
+        if not self.is_exponent(one_weight):
+            if self.target in one:
+                one_power = 1
+            else:
+                one_power = 0
+        else:
+            one_power = int(self.extract_exponent(one))
+        if not self.is_exponent(two_weight):
+            if self.target in two:
+                two_power = 1
+            else:
+                two_power = 0
+        else:
+            two_power = int(self.extract_exponent(two))
+        new_weight = str(int(one_weight)*int(two_weight))
+        new_power = str(one_power+two_power)
+        if new_power == "1":
+            return str(new_weight)+self.target
+        return str(new_weight)+self.target+"^("+str(new_power)+")"
+
     def derive_full(self):
         vals = self.extract_nums()
         new_vals = []
+        new_expression = ""
         for i in vals:
             new_vals.append(self.derive_one_num(i))
-        new_expression = new_vals[0]
         if len(new_vals) > 1:
             for i in range(1, len(new_vals)):
-                new_expression += self.operands[i-1] + new_vals[i]
+                if self.operands[i-1] in ["*", "/"]:
+                    new_expression += self.derivative_mul(new_vals[i], vals[i-1]) + "+" + self.derivative_mul(vals[i], new_vals[i-1])
+                else:
+                    new_expression += new_vals[i-1] + self.operands[i-1] + new_vals[i]
         return new_expression
