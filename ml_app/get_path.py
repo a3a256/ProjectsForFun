21f8741c45044bd20fb5e2beb04b352776ab215a
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-09-27 16:41:01 +0100

    added minor changes

--- ml_app/get_path.py
+++ ml_app/get_path.py
@@ -4,26 +4,31 @@ from tkinter.filedialog import askopenfile
 import os
 import tables
 import pandas as pd
+from tables import Nodd
+import ops
+import common_panel
 
-class BrowseFile:
+
+class BrowseFile(Nodd):
     def __init__(self, ui):
         self.ui = ui
-        self.ui.geometry('800x600')
-        self.cv = Canvas(width=800, height=600)
+        self.ui.geometry('300x300')
+        self.cv = Canvas(width=300, height=300)
         self.cv.pack(fill='both', expand=True)
+        self.df = None
+        self.tables = None
 
     def file_path(self):
         _filename = filedialog.askopenfilename()
         _filename = r'{}'.format(_filename)
-        df = pd.read_csv(_filename)
+        self.df = pd.read_csv(_filename)
         root = Tk()
-        t = tables.DataVis(root, df).pack(fill='both', expand=True)
+        self.tables = tables.DataVis(root, self.df, self.ui, self.cv).pack(fill='both', expand=True)
         root.mainloop()
 
-
     def port(self):
-        click = Button(master=self.ui, text='Open File', command=lambda: self.file_path())
-        self.cv.create_window(40, 10, window=click)
+        click = Button(master=self.ui, text='Open File', command=lambda: [self.ui.destroy(), self.file_path()])
+        self.cv.create_window(150, 150, window=click)
         self.ui.mainloop()
 
 
