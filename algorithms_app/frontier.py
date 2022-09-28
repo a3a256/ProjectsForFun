ac05e86f1bf992ba300e55dfb8ad9dc84a51fcd3
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-09-28 17:00:54 +0100

    added option to insert at id pos a value

--- algorithms_app/frontier.py
+++ algorithms_app/frontier.py
@@ -10,6 +10,19 @@ class ListVis:
         self.sll = lst.SLL(self.cv, first, 50, 70)
         self.intake = StringVar()
 
+    def go_insert(self, val, id):
+        self.sll.insert_at(val, int(id))
+        print(self.sll.show())
+
+    def inserting_at(self, val):
+        lb = Label(master=self.ui, text="Enter the index to insert at")
+        self.cv.create_window(900, 170, window=lb)
+        ent0 = Entry(master=self.ui, text="")
+        self.cv.create_window(900, 200, window=ent0)
+        btn = Button(master=self.ui, text="Insert!", command=lambda: [self.go_insert(val, ent0.get()), btn.destroy(), lb.destroy(), ent0.destroy()])
+        self.cv.create_window(900, 230, window=btn)
+        self.intake.set("")
+
     def adding_start(self, num):
         self.sll.add_start(int(num), 50, 70)
         self.intake.set('')
@@ -32,8 +45,10 @@ class ListVis:
         self.cv.create_window(900, 80, window=btn_start)
         btn_end = Button(master=self.ui, text='Add at end', command=lambda: self.adding_end(ent.get()))
         self.cv.create_window(900, 110, window=btn_end)
+        btn_insert = Button(master=self.ui, text="Insert at:", command=lambda: self.inserting_at(ent.get()))
+        self.cv.create_window(900, 140, window=btn_insert)
         btn_sort = Button(master=self.ui, text='Sort:ASC', command=lambda: self.asc_sorting())
-        self.cv.create_window(900, 140, window=btn_sort)
+        self.cv.create_window(900, 260, window=btn_sort)
         btn_sort_desc = Button(master=self.ui, text='Sort:DESC', command=lambda: self.desc_sorting())
-        self.cv.create_window(900, 170, window=btn_sort_desc)
+        self.cv.create_window(900, 290, window=btn_sort_desc)
         
