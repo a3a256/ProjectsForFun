316520d4f301fa54e8ea8bb8be8c924d1344f553
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-10-02 22:58:31 +0100

    added scatterplot option and is working for now

--- ml_app/ops.py
+++ ml_app/ops.py
@@ -2,7 +2,24 @@ from tkinter import *
 from tkinter import ttk, filedialog
 import matplotlib.pyplot as plt
 import pandas as pd
-import get_path
+import seaborn as sns
+
+class PlotPanel:
+    def __init__(self, ui, selection, df):
+        self.df = df
+        self.ui = ui
+        self.selection = selection
+
+    def scatter_plot(self):
+        plot = ShowPlots(self.df[self.selection[0]], self.df[self.selection[1]])
+        plot.scatterplot()
+    
+    def plot_options(self):
+        btn_scatter = Button(self.ui, text="Scatterplot of selected columns", command=lambda: self.scatter_plot())
+        btn_scatter.grid(row=0, column=0)
+        btn_bar = Button(self.ui, text="Barplot")
+        btn_bar.grid(row=1, column=0)
+        self.ui.mainloop()
 
 class ShowPlots:
     def __init__(self, x_axis=None, y_axis=None, legend=None):
