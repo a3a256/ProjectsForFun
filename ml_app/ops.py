a5bc89bb51cdfcaa08d5596ada9de1b60e5a7130
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-10-03 16:00:30 +0100

    added barplot function the order does not matter, option for more heights to be added

--- ml_app/ops.py
+++ ml_app/ops.py
@@ -3,6 +3,7 @@ from tkinter import ttk, filedialog
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
 class PlotPanel:
     def __init__(self, ui, selection, df):
@@ -13,11 +14,25 @@ class PlotPanel:
     def scatter_plot(self):
         plot = ShowPlots(self.df[self.selection[0]], self.df[self.selection[1]])
         plot.scatterplot()
+
+    def bar_plot(self):
+        if len(self.selection) > 2:
+            fig, ax = plt.subplots(nrows=1, ncols=len(self.selection), figsize=(10, 10))
+            for i in range(len(self.selection)):
+                ax[i].bar(self.df[self.selection[i]])
+        elif len(self.selection) == 2:
+            col1 = np.unique(self.df[self.selection[0]])
+            col2 = np.unique(self.df[self.selection[1]])
+            if len(col1) > len(col2):
+                plt.bar(self.df[self.selection[1]], self.df[self.selection[0]])
+            else:
+                plt.bar(self.df[self.selection[0]], self.df[self.selection[1]])
+        plt.show()
     
     def plot_options(self):
-        btn_scatter = Button(self.ui, text="Scatterplot of selected columns", command=lambda: self.scatter_plot())
+        btn_scatter = Button(self.ui, text="Scatterplot of selected columns", command=lambda: [self.ui.destroy() ,self.scatter_plot()])
         btn_scatter.grid(row=0, column=0)
-        btn_bar = Button(self.ui, text="Barplot")
+        btn_bar = Button(self.ui, text="Barplot", command=lambda: [self.ui.destroy(), self.bar_plot()])
         btn_bar.grid(row=1, column=0)
         self.ui.mainloop()
 
