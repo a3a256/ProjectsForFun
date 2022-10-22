78b262bd171b7603ce2e05808e4c193be763f458
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-10-22 23:52:19 +0100

    added part to select target

--- ml_app/ml_algorithms.py
+++ ml_app/ml_algorithms.py
@@ -5,11 +5,20 @@ class MLOptions:
         self.ui = ui
         self.path = path
         self.selection = selection
+        self.target = None
+
+    def give_target(self, target):
+        self.target = target
+
+    def target_select(self, option):
+        for i in range(len(self.selection)):
+            b = Button(master=self.ui, text=self.selection[i])
+            b.grid(row=0, column=i)
 
     def supervised(self):
         btn_classification = Button(master=self.ui, text="Classification")
         btn_classification.grid(row=0, column=0)
-        btn_regression = Button(master=self.ui, text="Regression")
+        btn_regression = Button(master=self.ui, text="Regression", command=lambda: [btn_classification.destroy(), btn_regression.destroy()])
         btn_regression.grid(row=1, column=0)
 
     def port(self):
