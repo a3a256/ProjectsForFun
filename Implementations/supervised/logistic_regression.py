ac6a7a5ceb52267a5eaa93bd10b7e861b1e1d605
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-10-24 14:15:51 +0100

    iteration and alpha added to be corrected 30% acc

--- Implementations/supervised/logistic_regression.py
+++ Implementations/supervised/logistic_regression.py
@@ -4,9 +4,10 @@ import math
 from sklearn.preprocessing import LabelEncoder
 import numpy.linalg as LA
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 class OneLogisticRegression:
-    def __init__(self, alpha=0.01):
+    def __init__(self, alpha=0.001):
         self.alpha = alpha
         self.b1 = 0
         self.b0 = 0
@@ -19,7 +20,7 @@ class OneLogisticRegression:
 
     def fit(self, x, y):
         k = 0
-        for _ in range(20):
+        for _ in range(100):
             initial = np.array([self.b0, self.b1])
             p = self.log_func(x, self.b1, self.b0)
             b0 = np.sum(y-p)
@@ -33,25 +34,34 @@ class OneLogisticRegression:
             if LA.det(information) != 0:
                 information = -LA.matrix_power(information, -1)
                 eq = initial - np.dot(LA.matrix_power(information, -1), u)
-            eq = initial - np.dot(information, u)
+            eq = initial - self.alpha*np.dot(information, u)
             self.b0 = eq[0]
             self.b1 = eq[1]
-        print(self.b0)
-        print(self.b1)
+
+
+    def prediction(self, x):
+        y = [0 if i < 0.5 else 1 for i in self.log_func(x, self.b1, self.b0)]
+        return y
 
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(r'C:\Users\Azamat.Ilyasov\OneDrive - Optomany Ltd\Desktop\new_sll\breast-cancer.csv')
+    df = pd.read_csv(r'...\breast-cancer.csv')
     df.drop("id", axis=1, inplace=True)
     le = LabelEncoder()
     le.fit(df.iloc[:, 0])
     df.iloc[:, 0] = le.transform(df.iloc[:, 0])
-    x = df.iloc[:, 1].values
-    y = df.iloc[:, 0].values
+    x = df.iloc[50:, 1].values
+    y = df.iloc[50:, 0].values
+    x_test = df.iloc[:50, 1].values
+    y_test = df.iloc[:50, 0].values
     lr = OneLogisticRegression()
     lr.fit(x.reshape(-1, 1), y)
     lg = LogisticRegression(fit_intercept=True)
     lg.fit(x.reshape(-1, 1), y)
     print(lg.coef_)
     print(lg.intercept_)
+    y_hat = lg.predict(x_test.reshape(-1, 1))
+    y_pred = lr.prediction(x_test)
+    print(accuracy_score(y_hat, y_test))
+    print(accuracy_score(y_pred, y_test))
