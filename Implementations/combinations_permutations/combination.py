d5610bcdbb57f261b96c1d75be39aeaabe48ea48
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-10-25 12:08:19 +0100

    added Pnr from statstics and combinatorics etc

--- Implementations/combinations_permutations/combination.py
+++ Implementations/combinations_permutations/combination.py
@@ -1,20 +1,26 @@
-class Combine:
+class CombinePermutation:
     def __init__(self, size_of_set, selected):
         self.size_of_set = size_of_set
         self.selected = selected
 
-    def permutate(self, val):
+    def factorial_count(self, val):
         num = 1
         for i in range(1, val+1):
             num *= i
         return num
 
-    def execute(self):
-        upper = self.permutate(self.size_of_set)
-        lower = self.permutate(self.size_of_set-self.selected)*self.permutate(self.selected)
+    def permute(self):
+        upper = self.factorial_count(self.size_of_set)
+        lower = self.factorial_count(self.size_of_set - self.selected)
+        return upper/lower
+
+    def combine(self):
+        upper = self.factorial_count(self.size_of_set)
+        lower = self.factorial_count(self.size_of_set-self.selected)*self.factorial_count(self.selected)
         return upper/lower
 
     
 if __name__ == "__main__":
-    cm = Combine(10, 8)
-    print(cm.execute())
+    cp = CombinePermutation(10, 8)
+    print(cp.combine())
+    print(cp.permute())
