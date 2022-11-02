59b1cab34f8b8ebeb14368966d4a73da7591c2ce
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-02 14:12:33 +0000

    added standard scaler for more than one feature

--- Implementations/standard_scaler.py
+++ Implementations/standard_scaler.py
@@ -6,18 +6,37 @@ class StandardScaler:
         self.max_x = None
 
     def fit(self, x):
-        self.min_x = min(x)
-        self.max_x = max(x)
+        if x.shape[1] > 1:
+            self.min_x = []
+            self.max_x = []
+            for i in range(x.shape[1]):
+                self.min_x += [min(x[:, i])]
+                self.max_x += [max(x[:, i])]
+        else:
+            self.min_x = min(x)
+            self.max_x = max(x)
+        print(self.min_x)
+        print(self.max_x)
 
     def transform(self, x):
-        for i in range(len(x)):
-            x[i] = (x[i]-self.min_x)/(self.max_x - self.min_x)
+        if x.shape[1] > 1:
+            for i in range(x.shape[1]):
+                for j in range(x.shape[0]):
+                    x[j, i] = (x[j, i]-self.min_x[i])/(self.max_x[i] - self.min_x[i])
+        else:
+            for i in range(len(x)):
+                x[i] = (x[i]-self.min_x)/(self.max_x-self.min_x)
         
         return x
 
     def inverse_transform(self, x):
-        for i in range(len(x)):
-            x[i] = (x[i]*(self.max_x-self.min_x)) + self.min_x
+        if x.shape[1] > 1:
+            for i in range(x.shape[1]):
+                for j in range(x.shape[0]):
+                    x[j, i] = (x[j, i]*(self.max_x[i]-self.min_x[i])) + self.min_x[i]
+        else:
+            for i in range(len(x)):
+                x[i] = (x[i]*(self.max_x-self.min_x)) + self.min_x
         
         return x
 
@@ -25,7 +44,7 @@ class StandardScaler:
 if __name__ == "__main__":
     df = pd.read_csv(r'ready_for_nn.csv')
     sc = StandardScaler()
-    sc.fit(df.iloc[:, 2].values)
-    print(df.iloc[:10, 2].values)
-    print(sc.transform(df.iloc[:10, 2].values))
-    print(sc.inverse_transform(df.iloc[:10, 2].values))
+    sc.fit(df.iloc[:, [0,2]].values)
+    print(df.iloc[:10, [0,2]].values)
+    print(sc.transform(df.iloc[:10, [0,2]].values))
+    print(sc.inverse_transform(sc.transform(df.iloc[:10, [0,2]].values)))
