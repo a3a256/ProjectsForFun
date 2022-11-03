c13fa38be51a1c84ba901d8165f67e0d693d0c01
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-03 17:12:00 +0000

    added scatterplot with classification

--- ml_app/supervised/classification/naive_bayes_model.py
+++ ml_app/supervised/classification/naive_bayes_model.py
@@ -2,6 +2,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+import numpy as np
+import sys
+sys.path.insert(0, r'C:\Users\Azamat.Ilyasov\OneDrive - Optomany Ltd\Desktop\ml_app')
+from dimensions import get_dimensions
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -20,6 +24,23 @@ class BayesClassifier:
 
     def test(self):
         y_hat = self.model.predict(self.x_test)
+        plt.figure(figsize=(5, 4))
         sns.heatmap(confusion_matrix(y_hat, self.y_test), annot=True)
         plt.title("GaussianNB: {}% accuracy".format(round(accuracy_score(y_hat, self.y_test)*100, 2)))
+        dims = get_dimensions(self.x_test.shape[1]-1)
+        width, height = dims
+        print(dims)
+        fig, axes = plt.subplots(ncols=width, nrows=height, figsize=(10, 4))
+        v = 0
+        plt.figure(figsize=(6, 3))
+        for i in np.unique(y_hat):
+            plt.scatter(self.x_test[y_hat==i, 0], self.x_test[y_hat==i, 1])
+        if height > 1:
+            for i in range(height):
+                for j in range(width):
+                    for k in np.unique(self.y_test):
+                        axes[i][j].scatter(self.x_test[y_hat==k, v], self.x_test[y_hat==k, -1])
+                    axes[i][j].legend(np.unique(self.y_test))
+                    v += 1
+        plt.tight_layout()
         plt.show()
