4e942099e2f0f94d3db665a69ce9a2e032d32365
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-06 23:59:29 +0000

    changed input

--- np_cpp/execute.cpp
+++ np_cpp/execute.cpp
@@ -6,5 +6,5 @@ int main(){
     int arr[5] = {0, 1, 2, 3, 4};
     int arr2[5] = {5, 6, 7, 8, 10};
     int size = sizeof(arr)/sizeof(arr[0]);
-    addition_1d(arr, arr2);
+    addition_1d(arr, arr2, size);
 }
