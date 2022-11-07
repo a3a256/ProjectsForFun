5259bd867b7c94ac23d133daa6ce5ca34a886c57
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-07 00:00:03 +0000

    changed internal functions

--- np_cpp/np_functions.cpp
+++ np_cpp/np_functions.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <iomanip>
 
-int* addition_1d(int arr[], int arr2[]){
-    int length1 = sizeof(arr)/sizeof(arr[0]);
-    int new_arr[length1];
-    for(int i = 0; i<length1; i++){
+int* addition_1d(int arr[], int arr2[], int size){
+    int new_arr[size];
+    for(int i = 0; i<size; i++){
         new_arr[i] = arr[i] + arr2[i];
         std::cout << new_arr[i] << "\n";
     }
