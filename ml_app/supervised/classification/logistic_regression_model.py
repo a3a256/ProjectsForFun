d9fc84a12e0cf8c912a4a18e83810e798e98f030
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-09 16:48:32 +0000

    I need to install statsmodel to run regplot for    Logistic Regression model

--- ml_app/supervised/classification/logistic_regression_model.py
+++ ml_app/supervised/classification/logistic_regression_model.py
@@ -27,25 +27,21 @@ class LogisticClassifier:
         plt.figure(figsize=(5, 4))
         sns.heatmap(confusion_matrix(y_hat, self.y_test), annot=True)
         plt.title("LogisticRegression: {}% accuracy".format(round(accuracy_score(y_hat, self.y_test)*100, 2)))
-        dims = get_dimensions(self.x_test.shape[1]-1)
+        dims = get_dimensions(self.x_test.shape[1])
         width, height = dims
-        print(dims)
         fig, axes = plt.subplots(ncols=width, nrows=height, figsize=(10, 4))
         v = 0
-        plt.figure(figsize=(6, 3))
-        for i in np.unique(y_hat):
-            plt.scatter(self.x_test[y_hat==i, 0], self.x_test[y_hat==i, 1])
         if height > 1:
             for i in range(height):
                 for j in range(width):
                     for k in np.unique(self.y_test):
-                        axes[i][j].scatter(self.x_test[y_hat==k, v], self.x_test[y_hat==k, -1])
+                        sns.regplot(x=self.x_test[:, v], y=self.y_test, ax=axes[i][j], logistic=True, ci=None)
                     axes[i][j].legend(np.unique(self.y_test))
                     v += 1
         else:
             for j in range(width):
                 for k in np.unique(self.y_test):
-                    axes[j].scatter(self.x_test[y_hat==k, v], self.x_test[y_hat==k, -1])
+                    sns.regplot(x=self.x_test[:, v], y=self.y_test, ax=axes[j], logistic=True, ci=None)
                 axes[j].legend(np.unique(self.y_test))
                 v += 1
         plt.tight_layout()
