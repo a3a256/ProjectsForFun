80f7ad63a4382c4ce216d1c8de06a593fa0685a4
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-12 23:49:33 +0000

    added call for transpose matrix

--- np_cpp/execute.cpp
+++ np_cpp/execute.cpp
@@ -6,5 +6,16 @@ int main(){
     int arr[5] = {0, 1, 2, 3, 4};
     int arr2[5] = {5, 6, 7, 8, 10};
     int size = sizeof(arr)/sizeof(arr[0]);
-    addition_1d(arr, arr2, size);
+    int* k;
+    k = addition_1d(arr, arr2, size);
+    std::cout << "check"<<std::endl;
+    for(int i = 0; i<size; i++){
+        std::cout << k[i] << std::endl;
+    }
+    int n[2][2] = {{1, 2}, {3, 4}};
+    int p[2][2] = {{5, 6}, {3, 9}};
+    int* l;
+    int rows = 2;
+    int cols = 2;
+    transpose_matrix(n, rows, cols);
 }
