47f8c2bb838b4587bb67bcf28f00684d866382b0
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-12 23:51:03 +0000

    fixed the mistake with the 1d arr

--- np_cpp/np_functions.cpp
+++ np_cpp/np_functions.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <iomanip>
 
+
+int** transpose_matrix(int arr[][1000], int rows, int columns){
+    int** new_arr = 0;
+    new_arr = new int*[rows];
+    for(int i = 0; i<columns; i++){
+        new_arr = new int*[columns];
+        for(int j =0; j<rows; j++){
+            new_arr[i][j] = arr[j][i];
+        }
+    }
+
+    return new_arr;
+}
+
+
 int* addition_1d(int arr[], int arr2[], int size){
-    int new_arr[size];
+
+    static int new_arr[1000];
     for(int i = 0; i<size; i++){
         new_arr[i] = arr[i] + arr2[i];
         std::cout << new_arr[i] << "\n";
