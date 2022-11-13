a0bd6e6182cfa6c18d2f893f9f1698a719f9e568
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-13 02:09:30 +0000

    added transpose and matrix multiplication

--- np_cpp/np_functions.cpp
+++ np_cpp/np_functions.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <iomanip>
+#include <cstdio>
+
+
+int** multiply_matrix(int arr1[][1000], int arr2[][1000], int rows1, int rows2, int cols1, int cols2){
+    static int** new_arr = 0;
+    new_arr = new int*[1000];
+    for(int i = 0; i<rows1; i++){
+        int a = 0;
+        new_arr[i] = new int[1000];
+        for(int j = 0; j<cols2; j++){
+            for(int k=0; k<cols1; k++){
+                a += arr1[i][k]*arr2[k][i];
+            }
+            new_arr[i][j] = a;
+        }
+    }
+    return new_arr;
+}
 
 
 int** transpose_matrix(int arr[][1000], int rows, int columns){
-    int** new_arr = 0;
-    new_arr = new int*[rows];
+    static int** new_arr = 0;
+    new_arr = new int*[1000];
     for(int i = 0; i<columns; i++){
-        new_arr = new int*[columns];
+        new_arr[i] = new int[1000];
         for(int j =0; j<rows; j++){
             new_arr[i][j] = arr[j][i];
         }
@@ -21,7 +39,6 @@ int* addition_1d(int arr[], int arr2[], int size){
     static int new_arr[1000];
     for(int i = 0; i<size; i++){
         new_arr[i] = arr[i] + arr2[i];
-        std::cout << new_arr[i] << "\n";
     }
     return new_arr;
 }
