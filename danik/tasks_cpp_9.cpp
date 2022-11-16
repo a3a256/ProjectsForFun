55310c6aa8d8b13c1e8bb4cf5f343162c19545b0
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-16 13:48:54 +0000

    complete 9

--- danik/tasks_cpp_9.cpp
+++ danik/tasks_cpp_9.cpp
@@ -161,7 +161,115 @@ void task8(){
     std::cout<<"\n";
 }
 
+void task7(){
+    int max=0;
+    int min=0;
+    int arr[] = {1, 5, 4, 6, 10, 12, 4, 5, 13, 8};
+    for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(arr[min]<arr[i]){
+            min = i;
+        }
+        if(arr[max]>arr[i]){
+            max = i;
+        }
+    }
+    int temp = arr[min];
+    arr[min] = arr[max];
+    arr[max] = temp;
+    for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        std::cout<<arr[i]<<" ";
+    }
+}
 
+void task6(){
+    int arr[] = {1, 5, 4, 6, 10, 12, 4, 5, 13, 8};
+    int odd;
+    for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(arr[i]%2 != 0){
+            odd = arr[i];
+        }
+    }
+    for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(arr[i]%2!=0){
+            arr[i]+= odd;
+        }
+        std::cout<<arr[i]<<" ";
+    }
+}
+
+void task5(){
+    int arr[] = {1, 5, 4, 6, 10, 12, 4, 5, 13, 19};
+    int k = arr[0];
+    for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(arr[i]%2==0){
+            arr[i] += k;
+        }
+        std::cout<<arr[i]<<" ";
+    }
+}
+
+void task4(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int k = 3;
+    for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        arr[i] += k;
+        std::cout << arr[i] <<" ";
+    }
+    std::cout << "\n";
+}
+
+void task3(){
+    int count = 0;
+    int arr[] = {1, 2, 6, 4, 9, 10, 11};
+    int arr1[sizeof(arr)/sizeof(arr[0])];
+    int j = 0;
+    for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(arr[i]%2 == 0){
+            arr1[j] = arr[i];
+            std::cout<<arr1[j]<<" ";
+            j += 1;
+        }
+    }
+    std::cout<<"\n"<<"The length is "<<j<<"\n";
+}
+
+void task2(){
+    int arr[] = {2, 13, 5, 9};
+    int arr1[] = {6, 1, 11, 8};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    int c[length];
+    for(int i = 0; i<length; i++){
+        if (arr[i]>arr1[i]){
+            c[i] = arr[i];
+        }else{
+            c[i] = arr1[i];
+        }
+    }
+    for(int i = 0; i<length; i++){
+        std::cout<<c[i]<<" ";
+    }
+    std::cout<<"\n";
+}
+
+void task1(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int arr1[] = {6, 7, 8, 9, 10};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    int temp;
+    for(int i = 0; i<length; i++){
+        temp = arr[i];
+        arr[i] = arr1[i];
+        arr1[i] = temp;
+    }
+    for(int i = 0; i<length; i++){
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"\n";
+    for(int i = 0; i<length; i++){
+        std::cout<<arr1[i]<<" ";
+    }
+    std::cout<<"\n";
+}
 
 int main()
 {
