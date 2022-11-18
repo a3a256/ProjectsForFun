1398a2dfebd086dbc795ad21487c027ac1b76d9c
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-11-18 15:32:04 +0000

    added z normalisation transform

--- Implementations/standard_scaler.py
+++ Implementations/standard_scaler.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
+def std(arr):
+    new_arr = []
+    mean = sum(arr)/len(arr)
+    for i in arr:
+        new_arr += [(i-mean)**2]
+    return (sum(new_arr)/len(arr))**(0.5)
+
+def mean(arr):
+    return sum(arr)/len(arr)
+
 class StandardScaler:
     def __init__(self):
         self.min_x = None
@@ -41,10 +51,42 @@ class StandardScaler:
         return x
 
 
+class ZNormalization:
+    def __init__(self):
+        self.stds = []
+        self.means = []
+
+    def fit(self, x):
+        n_samples, n_features = x.shape
+        if n_features > 1:
+            for i in range(n_features):
+                self.stds += [std(x[:, i])]
+                self.means += [mean(x[:, i])]
+        else:
+            self.stds += [std(x)]
+            self.mean += [mean(x)]
+        print(self.stds)
+        print(self.means)
+
+    def transform(self, x):
+        n_samples, n_features = x.shape
+        if n_features:
+            for i in range(n_samples):
+                for j in range(n_features):
+                    x[i, j] = (x[i, j]-self.means[j])/(self.stds[j])
+        return x
+
+
 if __name__ == "__main__":
     df = pd.read_csv(r'ready_for_nn.csv')
-    sc = StandardScaler()
+    # sc = StandardScaler()
+    # sc.fit(df.iloc[:, [0,2]].values)
+    # print(df.iloc[:10, [0,2]].values)
+    # print(sc.transform(df.iloc[:10, [0,2]].values))
+    # print(sc.inverse_transform(sc.transform(df.iloc[:10, [0,2]].values)))
+    sc = ZNormalization()
     sc.fit(df.iloc[:, [0,2]].values)
     print(df.iloc[:10, [0,2]].values)
+    sc.fit(df.iloc[:10, [0,2]].values)
     print(sc.transform(df.iloc[:10, [0,2]].values))
-    print(sc.inverse_transform(sc.transform(df.iloc[:10, [0,2]].values)))
+    # print(sc.inverse_transform(sc.transform(df.iloc[:10, [0,2]].values)))
