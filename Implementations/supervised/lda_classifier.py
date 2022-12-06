bc816b83e86ce3d49a6101c3cb2c1c9d040d9519
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-12-06 16:57:11 +0000

    added classifier using LDA
    referred to the paper 1906.02590 on arxiv
    Implemented model: 83% accuracy
    Real model: 89,5% accuracy

---
+++ Implementations/supervised/lda_classifier.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+class LDAClassifier:
+    def __init__(self):
+        self.covariances = []
+        self.means = []
+        self.priors = []
+
+    def fit(self, x, y):
+        counts = np.bincount(y)
+        self.priors = [counts[i]/len(x) for i in range(len(counts))]
+        for i in np.unique(y):
+            self.means += [np.mean(x[y==i, :], axis=0).reshape(-1, 1)]
+            self.covariances += [np.cov(x[y==i, :].T)]
+
+    def predict(self, x):
+        res = []
+        answers = []
+        for i in range(1):
+            kk = (self.means[0]-self.means[1]).T
+            first_part = np.dot(2*np.dot(np.linalg.inv(self.covariances[i]), (self.means[0] - self.means[1])).T, x.T)
+            second_part = np.dot(np.dot((self.means[0]-self.means[1]).T, np.linalg.inv(self.covariances[i])), (self.means[0]-self.means[1]))
+            end = 2*np.log(self.priors[1]/self.priors[0])
+            res += [first_part+second_part+end]
+            # answers += np.dot(2*np.dot(np.linalg.inv(self.covariances[i]), (self.means[i][0] - self.means[i][1])).T, x.T) + np.dot(np.dot((self.means[i][0]-self.means[i][1]).reshape(1, -1), np.linalg.inv(self.covariances[i])), (self.means[i][0]-self.means[i][1])) + 2*np.log(self.priors[0]/self.priors[1])
+        answers = [0 if i > 0 else 1 for i in res[0][0]]
+        return answers
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(r'breast-cancer.csv')
+    lda = LDAClassifier()
+    le = LabelEncoder()
+    sc = StandardScaler()
+    df['diagnosis'] = le.fit_transform(df['diagnosis'])
+    x = df.iloc[:, [2, 3]].values
+    x = sc.fit_transform(x)
+    y = df.iloc[:, 1].values
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
+    ld = LinearDiscriminantAnalysis()
+    ld.fit(x_train, y_train)
+    lda.fit(x_train, y_train)
+    yhat = lda.predict(x_test)
+    real = ld.predict(x_test)
+    print("Implemented:", accuracy_score(yhat, y_test))
+    print("Real: ", accuracy_score(real, y_test))
