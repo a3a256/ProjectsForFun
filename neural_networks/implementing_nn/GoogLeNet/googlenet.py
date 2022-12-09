495889855cb71111a5e696715e3d3ed89efda671
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-12-09 23:57:10 +0000

    added inception block, need to correct dimensions

--- neural_networks/implementing_nn/GoogLeNet/googlenet.py
+++ neural_networks/implementing_nn/GoogLeNet/googlenet.py
@@ -12,6 +12,7 @@ class GoogLeNet(nn.Module):
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.local_response_norm1 = nn.LocalResponseNorm(64)
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.inception1 = InceptionBlock(64)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -21,4 +22,49 @@ class GoogLeNet(nn.Module):
         x = self.conv3(x)
         x = self.local_response_norm1(x)
         x = self.pool2(x)
+        x = self.inception1(x)
         return F.relu(x)
+
+
+
+
+
+
+class InceptionBlock(nn.Module):
+    def __init__(self, in_channels):
+        super(InceptionBlock, self).__init__()
+        conv = Block
+        self.conv1_11 = conv(in_channels=in_channels, out_channels=in_channels+10, kernel=1)
+        self.conv1_12 = conv(in_channels=in_channels, out_channels=in_channels+10, kernel=1)
+        self.pool1_1 = conv(in_channels=in_channels, out_channels=in_channels+10, kernel=3, padding=1)
+        self.conv2_11 = conv(in_channels=in_channels, out_channels=in_channels+20, kernel=1)
+        self.conv2_31 = conv(in_channels=in_channels+10, out_channels=in_channels+20, kernel=3, padding=1)
+        self.conv2_51 = conv(in_channels=in_channels+10, out_channels=in_channels+20, kernel=5, padding=2)
+        self.conv2_12 = conv(in_channels=in_channels+10, out_channels=in_channels+20, kernel=1)
+
+    def forward(self, x):
+        first = self.conv2_11(x)
+        second = self.conv1_11(x)
+        second = self.conv2_31(second)
+        third = self.conv1_12(x)
+        third = self.conv2_51(third)
+        end = F.max_pool2d(self.pool1_1(x), kernel_size=3, padding=1)
+        print(end.shape)
+        end = self.conv2_12(end)
+        res = [first, second, third, end]
+        for i in res:
+            print(i.shape)
+        return torch.cat(res, 1)
+
+
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel, stride=1, padding=0):
+        super(Block, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel, stride=stride, padding=padding)
+        self.batch = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.batch(x)
+        return F.relu(x, inplace=True)
