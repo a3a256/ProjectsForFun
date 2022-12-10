a4d376d262b2e4800b288e89dfcd82d6aac0b42d
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-12-10 23:13:04 +0000

    fixed out channels added Auxiliary Classifier

--- neural_networks/implementing_nn/GoogLeNet/googlenet.py
+++ neural_networks/implementing_nn/GoogLeNet/googlenet.py
@@ -5,14 +5,18 @@ import torch.nn.functional as F
 class GoogLeNet(nn.Module):
     def __init__(self, n_classes):
         super(GoogLeNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=7, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=7, stride=2)
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.local_response_norm = nn.LocalResponseNorm(10)
-        self.conv2 = nn.Conv2d(in_channels=10, out_channels=32, kernel_size=1)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.local_response_norm1 = nn.LocalResponseNorm(64)
+        self.local_response_norm = nn.LocalResponseNorm(8)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=1)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=24, kernel_size=3, padding=1)
+        self.local_response_norm1 = nn.LocalResponseNorm(24)
         self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.inception1 = InceptionBlock(64)
+        self.inception1 = InceptionBlock(24, 32, 48)
+        self.inception2 = InceptionBlock(192, 128, 96)
+        self.inception3 = InceptionBlock(384, 192, 128)
+        self.inception4 = InceptionBlock(512, 192, 64)
+        self.auxiliary1 = AuxiliaryClassifier(512, 192)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -23,7 +27,13 @@ class GoogLeNet(nn.Module):
         x = self.local_response_norm1(x)
         x = self.pool2(x)
         x = self.inception1(x)
-        return F.relu(x)
+        x =self.inception2(x)
+        x = self.inception3(x)
+        softmax0 = self.auxiliary1(x)
+        x = self.inception4(x)
+        print(x.shape)
+        print(softmax0.shape)
+        return softmax0, F.relu(x)
 
 
 
@@ -31,16 +41,16 @@ class GoogLeNet(nn.Module):
 
 
 class InceptionBlock(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, mid_channels, out_channels):
         super(InceptionBlock, self).__init__()
         conv = Block
-        self.conv1_11 = conv(in_channels=in_channels, out_channels=in_channels+10, kernel=1)
-        self.conv1_12 = conv(in_channels=in_channels, out_channels=in_channels+10, kernel=1)
-        self.pool1_1 = conv(in_channels=in_channels, out_channels=in_channels+10, kernel=3, stride=1, padding=1)
-        self.conv2_11 = conv(in_channels=in_channels, out_channels=in_channels+20, kernel=1)
-        self.conv2_31 = conv(in_channels=in_channels+10, out_channels=in_channels+20, kernel=3, padding=1)
-        self.conv2_51 = conv(in_channels=in_channels+10, out_channels=in_channels+20, kernel=5, padding=2)
-        self.conv2_12 = conv(in_channels=in_channels+10, out_channels=in_channels+20, kernel=1)
+        self.conv1_11 = conv(in_channels=in_channels, out_channels=mid_channels, kernel=1)
+        self.conv1_12 = conv(in_channels=in_channels, out_channels=mid_channels, kernel=1)
+        self.pool1_1 = conv(in_channels=in_channels, out_channels=mid_channels, kernel=3, stride=1, padding=1)
+        self.conv2_11 = conv(in_channels=in_channels, out_channels=out_channels, kernel=1)
+        self.conv2_31 = conv(in_channels=mid_channels, out_channels=out_channels, kernel=3, padding=1)
+        self.conv2_51 = conv(in_channels=mid_channels, out_channels=out_channels, kernel=5, padding=2)
+        self.conv2_12 = conv(in_channels=mid_channels, out_channels=out_channels, kernel=1)
 
     def forward(self, x):
         first = self.conv2_11(x)
@@ -52,6 +62,27 @@ class InceptionBlock(nn.Module):
         end = self.conv2_12(end)
         res = [first, second, third, end]
         return torch.cat(res, 1)
+
+
+
+class AuxiliaryClassifier(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(AuxiliaryClassifier, self).__init__()
+        conv = Block
+        self.pool = conv(in_channels, out_channels, 5, padding=2)
+        self.conv1 = conv(out_channels, out_channels+16, 1)
+        self.fc = nn.Linear(720, 1000)
+        self.fc1 = nn.Linear(1000, 1)
+
+    def forward(self, x):
+        print(x.shape)
+        x = F.avg_pool2d(self.pool(x), kernel_size=5, stride=1, padding=1)
+        x = self.conv1(x)
+        adapt = nn.AdaptiveAvgPool2d(720)
+        x = adapt(x)
+        x = self.fc(x)
+        x = self.fc1(x)
+        return F.softmax(x, dim=2)
 
 
 
