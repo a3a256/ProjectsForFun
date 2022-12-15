79fa56240584fba1c175b072acffd0b7a7cb4476
a3amat1K <81564718+a3a256@users.noreply.github.com>
2022-12-15 16:40:29 +0000

    adam optimizer
    Added adam optimizer to SVC algorithm.
    lr 0.01: 0.85
    lr 0.001: 0.9
    Performs just as good as with simple SGD

--- Implementations/supervised/svc.py
+++ Implementations/supervised/svc.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 class SVM:
-    def __init__(self, C=0.001, alpha=0.01, fit_intercept=False, n_iters=100):
+    def __init__(self, C=0.001, alpha=0.001, fit_intercept=False, n_iters=100):
         self.alpha = alpha
         self.C = C
         self.w = None
@@ -35,9 +35,36 @@ class SVM:
                 self.w -= self.alpha*grad
                 if self.intercept:
                     self.bias -= self.alpha*(1/n_samples) * np.sum(error)
+    
+    def fit_adam(self, x, y):
+        b1 = 0.999
+        b2 = 0.9
+        n_samples, n_features = x.shape
+        if self.intercept:
+            self.bias = np.random.rand(1)
+        self.w = np.random.rand(n_features)
+        
+        for _ in range(self.iter):
+            m0 = 0
+            v0 = 0
+            t = 0
+            for i in range(n_samples):
+                t += 1
+                yhat = self.equation(x[i, :], self.w, self.bias)
+                error = 1 - y[i]*yhat
+                grad = 0
+                if error <= 0:
+                    grad = self.w
+                else:
+                    grad = self.w - self.C*y[i]*x[i, :]
+                m0 = b1*m0 + (1-b1)*grad
+                v0 = b2*v0 + (1-b2)*np.square(grad)
+                mhat = m0/(1-b1**t)
+                vhat = v0/(1-b2**t)
+                self.w -= (self.alpha*mhat)/(np.sqrt(vhat)+10**(-8))
+                if self.intercept:
+                    self.bias -= self.alpha*(1/n_samples) * np.sum(error)
 
-
-        print(self.w)
 
     def equation(self, x, w, b=None):
         if self.intercept:
@@ -54,7 +81,6 @@ class SVM:
                 predict[i] = -1
             elif predict[i] > 0:
                 predict[i] = 1
-        print(predict)
         return predict
 
 
@@ -74,9 +100,8 @@ if __name__ == "__main__":
     y = df.iloc[:, 0].values
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.2)
     sv = SVM()
-    sv.fit(x_train, y_train)
+    sv.fit_adam(x_train, y_train)
     print("Implemented: {}".format(accuracy_score(y_test, sv.predict(x_test))))
     svc = SVC(C=0.001, kernel="linear")
     svc.fit(x_train, y_train)
-    print(svc.coef_)
     print("Real SVC: {}".format(accuracy_score(y_test, svc.predict(x_test))))
