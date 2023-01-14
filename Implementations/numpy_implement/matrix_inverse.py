8381da18e9768629e25b3159c9c33a137c0da98f
a3amat1K <81564718+a3a256@users.noreply.github.com>
2023-01-14 23:37:11 +0000

    implemented for full
    But not correct. Implemented following intuitive approach

--- Implementations/numpy_implement/matrix_inverse.py
+++ Implementations/numpy_implement/matrix_inverse.py
@@ -2,7 +2,9 @@ import numpy as np
 
 matrix = [[1.2, 3.1, 6.7], [5.3, 6.6, 1.9], [4.5, 7.2, 8.9]]
 
-# print(np.linalg.inv(matrix))
+print(np.array(matrix))
+
+print(np.linalg.inv(matrix))
 
 def eye(size):
     arr = []
@@ -25,32 +27,64 @@ def is_diagonal(arr):
                     return False
     return True
 
-def row_closest(arr, row, column):
+def row_closest(arr, row, column, ie):
     close_row = None
     close_column = None
     dd = dict()
     for i in range(len(arr)):
         if i != row:
             diff = arr[row][column] - arr[i][column]
-            if abs(diff) not in dd:
+            if diff not in dd:
                 if arr[i][column] != 0:
-                    dd[abs(diff)] = [[i, column]]
+                    dd[diff] = [[i, column]]
             else:
                 if arr[i][column] != 0:
-                    dd[abs(diff)] += [[i, column]]
+                    dd[diff] += [[i, column]]
 
     sorted_keys = sorted(dd, key=lambda x: x)
+    print(sorted_keys)
+    index = sorted_keys[0]
+
+    close_row = dd[index][0][0]
+
+    close_column = dd[index][0][1]
+
+    # print(close_column)
+
+    value = arr[close_row][close_column]
+
+    coef = arr[row][column]/value if value > arr[row][column] else value/arr[row][column]
+
+    if arr[row][column] > 0 and value > 0:
+        coef *= -1
+    elif arr[row][column] < 0 and value < 0:
+        coef *= -1
+
+    temp_real = [arr[close_row][k]*coef for k in range(len(arr))]
+
+    temp_eye = [ie[close_row][k]*coef for k in range(len(arr))]
+
+    for k in range(len(arr[0])):
+        ie[row][k] += temp_eye[k]
+        arr[row][k] += temp_real[k]
+
+    return arr, ie
 
 def augment(arr, ide):
     for i in range(len(arr)):
         for j in range(len(arr[0])):
             if i != j:
                 if arr[i][j] != 0:
-                    pass
+                    arr, ide = row_closest(arr, i, j, ide)
+                    print(ide)
+
+    return ide
 
 def inverse(arr):
     identity = eye(len(arr))
-    for i in range(len(arr)):
-        for j in range(len(arr[0])):
-            while not is_diagonal(arr):
-                pass
+    return augment(arr, identity)
+
+
+ans = inverse(matrix)
+
+print(np.array(ans))
