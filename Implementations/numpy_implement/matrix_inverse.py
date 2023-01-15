681e3f98e6f08a3ce1823b00e3e711c16939937d
a3amat1K <81564718+a3a256@users.noreply.github.com>
2023-01-15 09:37:08 +0000

    added new method and it works
    Implemented computation of inverse of a matrix using cofactor method

--- Implementations/numpy_implement/matrix_inverse.py
+++ Implementations/numpy_implement/matrix_inverse.py
@@ -34,15 +34,15 @@ def row_closest(arr, row, column, ie):
     for i in range(len(arr)):
         if i != row:
             diff = arr[row][column] - arr[i][column]
-            if diff not in dd:
+            if abs(diff) not in dd:
                 if arr[i][column] != 0:
-                    dd[diff] = [[i, column]]
+                    dd[abs(diff)] = [[i, column]]
             else:
                 if arr[i][column] != 0:
-                    dd[diff] += [[i, column]]
+                    dd[abs(diff)] += [[i, column]]
 
     sorted_keys = sorted(dd, key=lambda x: x)
-    print(sorted_keys)
+    # print("ff" ,dd)
     index = sorted_keys[0]
 
     close_row = dd[index][0][0]
@@ -63,11 +63,10 @@ def row_closest(arr, row, column, ie):
     temp_real = [arr[close_row][k]*coef for k in range(len(arr))]
 
     temp_eye = [ie[close_row][k]*coef for k in range(len(arr))]
-
     for k in range(len(arr[0])):
         ie[row][k] += temp_eye[k]
         arr[row][k] += temp_real[k]
-
+    # print(ie)
     return arr, ie
 
 def augment(arr, ide):
@@ -76,7 +75,7 @@ def augment(arr, ide):
             if i != j:
                 if arr[i][j] != 0:
                     arr, ide = row_closest(arr, i, j, ide)
-                    print(ide)
+                    # print(ide)
 
     return ide
 
@@ -84,7 +83,59 @@ def inverse(arr):
     identity = eye(len(arr))
     return augment(arr, identity)
 
+def determinant(arr):
+    if len(arr) == 1:
+        return arr[0][0]
+    det = 0
+    coef = -1
+    for i in range(len(arr)):
+        new_arr = []
+        for j in range(1, len(arr)):
+            temp = []
+            for k in range(len(arr)):
+                if k != i:
+                    temp += [arr[j][k]]
+            new_arr += [temp]
+        det += arr[0][i]*coef**(1+(i+1))*determinant(new_arr)
 
-ans = inverse(matrix)
+    return det
+
+def find_cofactor(arr):
+    new_matrix = []
+    for i in range(len(arr)):
+        row = []
+        for j in range(len(arr)):
+            temp_matrix = []
+            for z in range(len(arr)):
+                if z != i:
+                    temp_row = []
+                    for k in range(len(arr)):
+                        if k != j:
+                            temp_row += [arr[z][k]]
+                    temp_matrix += [temp_row]
+            row += [determinant(temp_matrix)]
+        new_matrix += [row]
+    return new_matrix
+
+def transpose(arr):
+    new_matrix = []
+    for i in range(len(arr[0])):
+        temp = []
+        for j in range(len(arr)):
+            temp += [arr[j][i]]
+        new_matrix += [temp]
+    return new_matrix
+
+def cofactor_inverse(arr):
+    arr_d = determinant(arr)
+    cofactor = transpose(find_cofactor(arr))
+    for i in range(len(cofactor)):
+        for j in range(len(cofactor[0])):
+            cofactor[i][j] /= arr_d
+
+    return cofactor
+
+
+ans = cofactor_inverse(matrix)
 
 print(np.array(ans))
