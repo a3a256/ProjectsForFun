b4ef3fda250a45efd4ef074990df384dece03a9d
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-09 19:52:23 +0100

    Implemented binary search

--- js_basics/js_search.js
+++ js_basics/js_search.js
@@ -1,3 +1,11 @@
 function binary_search(arr, start, end, target){
+    var mid = parseInt(start+end);
+    if(arr[mid] == target){
+        return mid;
+    }else if(arr[mid] > target){
+        return binary_search(arr, start, mid-1, target);
+    }else if(arr[mid] < target){
+        return binary_search(arr, mid+1, end, target);
+    }
     return -1;
 }
