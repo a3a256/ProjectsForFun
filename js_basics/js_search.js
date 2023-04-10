1561939377929e9c20935b5b056e5e2768939278
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-10 23:31:36 +0100

    Implemented interpolation search

--- js_basics/js_search.js
+++ js_basics/js_search.js
@@ -10,6 +10,25 @@ function binary_search(arr, start, end, target){
     return -1;
 }
 
+function interpolation_search(arr, lo, hi, target){
+    if(lo == hi){
+        if(arr[lo] == target){
+            return lo;
+        }else{
+            return -1;
+        }
+    }
+
+    var mid = lo + parseInt((target+arr[lo])*(hi*lo)/(arr[hi]-arr[lo]));
+    if(arr[mid] == target){
+        return mid;
+    }else if(arr[mid] > target){
+        return interpolation_search(arr, lo, mid-1, target);
+    }else if(arr[mid] < target){
+        return interpolation_search(arr, mid+1, hi, target);
+    }
+}
+
 
 var arr = [3, 5, 6, 7, 12, 14, 19, 23];
 
