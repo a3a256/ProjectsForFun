45ca0082584af1d1d95ef540a6bc3dd57959d350
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-15 04:21:19 +0100

    insert and class

--- js_basics/sll.js
+++ js_basics/sll.js
@@ -4,3 +4,24 @@ class Node{
         this.next = null;
     }
 }
+
+
+class SLL{
+    constructor(num){
+        this.head = Node(num);
+    }
+
+
+    insert_at_start = function(num){
+        var node = Node(num);
+        if(head == null){
+            head = Node(num);
+            return;
+        }
+
+        node.next = head;
+
+        head = Node(node.val);
+        head.next = node.next;
+    }
+}
