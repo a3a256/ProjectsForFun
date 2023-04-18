7a969fa2cbbb9f4134c72d97cc37010b9935fb46
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-19 00:55:38 +0100

    add on the left

--- js_basics/bst.js
+++ js_basics/bst.js
@@ -5,3 +5,27 @@ class Node{
         this.right = null;
     }
 }
+
+
+class BST{
+    constructor(num){
+        this.root = num;
+        this.left = null;
+        this.right = null;
+    }
+
+    add = function(num){
+        var head = this.root;
+        if(head == null){
+            this.root = new BST(num);
+        }else{
+            if(num > this.root.val){
+                if(this.root.right != null){
+                    this.left.add(num);
+                }else{
+                    this.left = new BST(num);
+                }
+            }
+        }
+    }
+}
