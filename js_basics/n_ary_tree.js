1047f2db440609c2cf745886c534adde59b35752
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-30 03:08:10 +0100

    function to add to n ary tree

--- js_basics/n_ary_tree.js
+++ js_basics/n_ary_tree.js
@@ -3,4 +3,26 @@ class NTree{
         this.root = num;
         this.children = [];
     }
+
+    add = function(num){
+        if(this.root == null){
+            this.root = new NTree(num);
+            return;
+        }
+
+        const prompt = require('prompt-sync')();
+        var name = prompt('Do you want to go to another level?(If no - node will be added to the current level');
+        if(name == 'n'){
+            this.children.push(new NTree(num));
+        }else{
+            for(let i=0; i<this.children.length; i++){
+                var val = 'Do you want to add node to index ' + i.toString();
+                name = prompt(val);
+                if(name == 'y'){
+                    this.children[i].add(num);
+                    return;
+                }
+            }
+        }
+    }
 }
