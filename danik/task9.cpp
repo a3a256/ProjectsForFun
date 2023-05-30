e9034b563fe5e84bc7e9d619ce52e6c9df0eb2a7
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-05-31 00:36:19 +0100

    added not tested

---
+++ danik/task9.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main(){
+    int op1 = 50;
+    int op2 = 40;
+    int op3 = 30;
+    int discount1 = 10;
+    int discount2 = 20;
+    int discount3 = 30;
+    int min1, min2, min3;
+    int wmin1, wmin2, wmin3;
+    std::cout << "Minutes during weekdays for operator 1: ";
+    std::cin >> min1;
+    std::cout << "\nTotal price for weekdays " << min1*op1;
+    std::cout << "\nMinutes during weekdays for operator 2: ";
+    std::cin >> min2;
+    std::cout << "\nTotal price for weekdays for operator 2: " << min2*op2;
+    std::cout << "\nMinutes during weekdays for operator 3: ";
+    std::cin >> min3;
+    std::cout << "\nTotal price for weekdays for operator 3: " << min3*op3;
+    std::cout << "\nMinutes during weekends for operator 1: ";
+    std::cin >> wmin1;
+    std::cout << "\nTotal price for weekends " << (float)wmin1*(float)op1*((float)(100-discount1)/100);
+    std::cout << "\nMinutes during weekends for operator 2: ";
+    std::cin >> wmin2;
+    std::cout << "\nTotal price for weekends for operator 2: " << (float)wmin2*(float)op2*((float)(100-discount2)/100);
+    std::cout << "\nMinutes during weekends for operator 3: ";
+    std::cin >> wmin3;
+    std::cout << "\nTotal price for weekends for operator 3: " << (float)wmin3*(float)op3*((float)(100-discount3)/100);
+}
