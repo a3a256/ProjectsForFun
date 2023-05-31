9abf18933387d45850ae5673c60394cc3d9f456c
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-06-01 00:47:41 +0100

    added some math

---
+++ danik/task9_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+
+
+int main(){
+    float a, b, x, z;
+    float first = a*pow(sin(x), 2) + b*cos(z*x+a);
+    float second = pow((a+b*x), 2) - sin(a+z*x);
+    float third = sqrt(x-(sin(b*x+z)));
+}
