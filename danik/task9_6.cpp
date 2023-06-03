3e64f8a28b2d191327a1307e12653f76458edf51
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-06-04 00:54:02 +0100

    implemented task

--- danik/task9_6.cpp
+++ danik/task9_6.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
+int main(){
+
+    std::vector<char> arr;
+    std::string value = "Hello World! ";
+    int i;
+    for(i=0; i<value.size(); i++){
+        arr.push_back(value[i]);
+    }
+
+    std::string res = "";
+
+    for(i=0; i<arr.size(); i++){
+        std::cout << arr[i];
+        if(arr[i] == ' '){
+            res += arr[i];
+            res += ' ';
+        }else{
+            res += arr[i];
+        }
+    }
+
+    std::cout << "\n" << res << "end\n";
+
+
+    return 0;
+}
